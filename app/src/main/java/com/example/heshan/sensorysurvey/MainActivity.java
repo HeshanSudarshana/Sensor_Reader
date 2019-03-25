deefdfdd2164f46a2262b9bed85071cd599fbb8c
HeshanSudarshana <heshan.15@cse.mrt.ac.lk>
2019-03-25 21:46:47 +0530

    sensors added

--- app/src/main/java/com/example/heshan/sensorysurvey/MainActivity.java
+++ app/src/main/java/com/example/heshan/sensorysurvey/MainActivity.java
@@ -19,10 +19,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     // Proximity and light sensors, as retrieved from the sensor manager.
     private Sensor mSensorProximity;
     private Sensor mSensorLight;
+    private Sensor mAccelerometer;
+    private Sensor mGyroscope;
+    private Sensor mOrientation;
+    private Sensor mMagneticField;
 
     // TextViews to display current sensor values.
     private TextView mTextSensorLight;
     private TextView mTextSensorProximity;
+    private TextView mTextAccelerometer;
+    private TextView mTextGyroscope;
+    private TextView mTextOrientation;
+    private TextView mTextMagneticField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Initialize all view variables.
         mTextSensorLight = findViewById(R.id.label_light);
         mTextSensorProximity = findViewById(R.id.label_proximity);
+        mTextAccelerometer = findViewById(R.id.label_accelerometer);
+        mTextGyroscope = findViewById(R.id.label_gyroscope);
+        mTextOrientation = findViewById(R.id.label_orientation);
+        mTextMagneticField = findViewById(R.id.label_magnetic_field);
 
         // Get an instance of the sensor manager.
         mSensorManager = (SensorManager) getSystemService(
@@ -40,9 +52,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Get light and proximity sensors from the sensor manager.
         // The getDefaultSensor() method returns null if the sensor
         // is not available on the device.
-        mSensorProximity = mSensorManager.getDefaultSensor(
-                Sensor.TYPE_PROXIMITY);
+        mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         // Get the error message from string resources.
         String sensor_error = getResources().getString(R.string.error_no_sensor);
@@ -50,9 +65,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // If either mSensorLight or mSensorProximity are null, those sensors
         // are not available in the device.  Set the text to the error message
         if (mSensorLight == null) { mTextSensorLight.setText(sensor_error); }
-        if (mSensorProximity == null) {
-            mTextSensorProximity.setText(sensor_error);
-        }
+        if (mSensorProximity == null) { mTextSensorProximity.setText(sensor_error); }
+        if (mAccelerometer == null) { mTextAccelerometer.setText(sensor_error); }
+        if (mGyroscope == null) { mTextGyroscope.setText(sensor_error); }
+        if (mOrientation == null) { mTextOrientation.setText(sensor_error); }
+        if (mMagneticField == null) { mTextMagneticField.setText(sensor_error); }
     }
 
     @Override
@@ -71,6 +88,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
         if (mSensorLight != null) {
             mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mAccelerometer != null) {
+            mSensorManager.registerListener(this, mAccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mGyroscope != null) {
+            mSensorManager.registerListener(this, mGyroscope,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mOrientation != null) {
+            mSensorManager.registerListener(this, mOrientation,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mMagneticField != null) {
+            mSensorManager.registerListener(this, mMagneticField,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
@@ -93,7 +126,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // The new data value of the sensor.  Both the light and proximity
         // sensors report one value at a time, which is always the first
         // element in the values array.
-        float currentValue = sensorEvent.values[0];
+        float[] currentValue = sensorEvent.values;
 
         switch (sensorType) {
             // Event came from the light sensor.
@@ -101,13 +134,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 // Set the light sensor text view to the light sensor string
                 // from the resources, with the placeholder filled in.
                 mTextSensorLight.setText(getResources().getString(
-                        R.string.label_light, currentValue));
+                        R.string.label_light, currentValue[0]));
                 break;
             case Sensor.TYPE_PROXIMITY:
                 // Set the proximity sensor text view to the light sensor
                 // string from the resources, with the placeholder filled in.
                 mTextSensorProximity.setText(getResources().getString(
-                        R.string.label_proximity, currentValue));
+                        R.string.label_proximity, currentValue[0]));
+                break;
+            case Sensor.TYPE_ACCELEROMETER:
+                // Set the proximity sensor text view to the light sensor
+                // string from the resources, with the placeholder filled in.
+                mTextAccelerometer.setText(getResources().getString(
+                        R.string.label_accelerometer, currentValue[0], currentValue[1], currentValue[2]));
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                // Set the proximity sensor text view to the light sensor
+                // string from the resources, with the placeholder filled in.
+                mTextGyroscope.setText(getResources().getString(
+                        R.string.label_gyroscope, currentValue[0], currentValue[1], currentValue[2]));
+                break;
+            case Sensor.TYPE_ORIENTATION:
+                // Set the proximity sensor text view to the light sensor
+                // string from the resources, with the placeholder filled in.
+                mTextOrientation.setText(getResources().getString(
+                        R.string.label_orientation, currentValue[0], currentValue[1], currentValue[2]));
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                // Set the proximity sensor text view to the light sensor
+                // string from the resources, with the placeholder filled in.
+                mTextMagneticField.setText(getResources().getString(
+                        R.string.label_magnetic_field, currentValue[0], currentValue[1], currentValue[2]));
                 break;
             default:
                 // do nothing
